3e5c61aee2b8e82385f556b19bff575b4fd51711
akilinjamam <akilinjamam@gmail.comm>
2022-10-05 19:20:50 +0600

    file-updated

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import './Calculator.css'
 
-
 const Calculator = () => {
-
     const one = '1';
     const two = '2';
     const three = '3';
@@ -15,76 +13,68 @@ const Calculator = () => {
     const nine = '9';
     const zero = '0';
     const point = '.';
-
+    // symbols
     const plus = '+';
     const minus = '-';
     const multiply = '*';
     const devide = '/';
+    const percent = '%'
 
     let [total, setTotal] = useState('')
     let [secondTotal, setSecondTotal] = useState('')
     const [symboles, setSymboles] = useState('')
+    const [parcentage, setParcentage] = useState()
+    const [percentSymbol, setPercentSymbol] = useState()
     const [calc, setCalc] = useState()
-    console.log(calc)
-    const [active, setActive] = useState(false)
-    console.log(symboles)
-
 
     const setSymbole = (symbole) => {
         setSymboles(symbole)
     }
 
-
-
-    const parsedTotal = parseInt(total)
-    const parsedSecondTotal = parseInt(secondTotal)
-
-
+    const parsedTotal = parseFloat(total)
+    const parsedSecondTotal = parseFloat(secondTotal)
 
     const setNumber = (number) => {
-
         if (!secondTotal && !symboles) {
             const numbers = total + number
             setTotal(numbers)
         }
-
-
-
         if (total && symboles) {
             const numbers = secondTotal + number
             setSecondTotal(numbers)
         }
-
-
-
-
     }
 
     const del = () => {
-
         setTotal('')
         setSecondTotal('')
         setSymboles()
-        setActive(false)
         setCalc('')
+        setPercentSymbol('')
+        setParcentage('')
+        setPercentSymbol('')
     }
 
     const backSpace = () => {
         if (total) {
             setTotal(total.slice(0, total.length - 1))
         }
-
-
         if (symboles) {
             setSymboles('')
             setTotal(total)
         }
-
         if (secondTotal) {
             setSecondTotal(secondTotal.slice(0, secondTotal.length - 1))
             setTotal(total)
             setSymboles(symboles)
             setCalc('')
+
+        }
+        if (percentSymbol) {
+            setPercentSymbol('')
+            setSecondTotal(secondTotal)
+            setCalc('')
+            setParcentage('')
         }
     }
 
@@ -109,74 +99,45 @@ const Calculator = () => {
 
     }
 
+    const setparcentage = (percent) => {
+
+        if (percent) {
+            const total = calc / 100
+            setParcentage(total)
+            setPercentSymbol(percent)
+        }
+    }
+
     return (
         <div className='body'>
 
 
-            <div className='main' >
-                {total} {total && symboles} {secondTotal}  {<p> {calc}</p>}
+            <div className='display' >
+                {total} {total && symboles} {secondTotal} {percentSymbol}   {<p>  {parcentage ? parcentage : calc} </p>}
             </div>
 
             <div className='button-side' >
-                <div>
-                    <button onClick={() => setNumber(seven)}>7</button>
-                </div>
-                <div>
-                    <button onClick={() => setNumber(eight)}>8</button>
-                </div>
-                <div>
-                    <button onClick={() => setNumber(nine)}>9</button>
-                </div>
-                <div>
-                    <button onClick={() => setNumber(four)}>4</button>
-                </div>
-                <div>
-                    <button onClick={() => setNumber(five)}>5</button>
-                </div>
-                <div>
-                    <button onClick={() => setNumber(six)}>6</button>
-                </div>
 
-                <div>
-                    <button onClick={() => setNumber(one)}>1</button>
-                </div>
-                <div>
-                    <button onClick={() => setNumber(two)}>2</button>
-                </div>
-                <div>
-                    <button onClick={() => setNumber(three)}>3</button>
-                </div>
-                <div>
-                    <button onClick={() => setNumber(point)}>.</button>
-                </div>
-                <div >
-                    <button onClick={() => setNumber(zero)}>0</button>
-                </div>
-
-                <div>
-                    <button className='orange' disabled={calc} onClick={totalCalculation}>=</button>
-                </div>
-                <div>
-                    <button className='symbol' onClick={() => setSymbole(plus)}>+</button>
-                </div>
-                <div>
-                    <button className='symbol' onClick={() => setSymbole(minus)}>-</button>
-                </div>
-                <div>
-                    <button className='symbol' disabled={!total} onClick={() => setSymbole(multiply)}>*</button>
-                </div>
-                <div>
-                    <button className='symbol' disabled={!total} onClick={() => setSymbole(devide)}>/</button>
-                </div>
-                <div>
-                    <button className='red' onClick={del}>del</button>
-                </div>
-                <div>
-                    <button className='red' onClick={backSpace}> Back </button>
-                </div>
+                <button className='red ' id='clear' onClick={del}>Clear</button>
+                <button className='red' onClick={backSpace}> C </button>
+                <button className='symbol' disabled={!total} onClick={() => setSymbole(devide)}>&divide;</button>
+                <button onClick={() => setNumber(seven)}>7</button>
+                <button onClick={() => setNumber(eight)}>8</button>
+                <button onClick={() => setNumber(nine)}>9</button>
+                <button className='symbol' disabled={!total} onClick={() => setSymbole(multiply)}>&times;</button>
+                <button onClick={() => setNumber(four)}>4</button>
+                <button onClick={() => setNumber(five)}>5</button>
+                <button onClick={() => setNumber(six)}>6</button>
+                <button className='symbol' onClick={() => setSymbole(minus)}>-</button>
+                <button onClick={() => setNumber(one)}>1</button>
+                <button onClick={() => setNumber(two)}>2</button>
+                <button onClick={() => setNumber(three)}>3</button>
+                <button className='symbol' onClick={() => setSymbole(plus)}>+</button>
+                <button onClick={() => setNumber(point)}>.</button>
+                <button onClick={() => setNumber(zero)}>0</button>
+                <button className='orange' disabled={calc} onClick={totalCalculation}>=</button>
+                <button onClick={() => setparcentage(percent)} className='symbol'>%</button>
             </div>
-
-
         </div>
     );
 };
